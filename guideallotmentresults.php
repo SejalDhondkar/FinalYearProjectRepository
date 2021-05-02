617d91805af595fe292ee24ed38751239af6fe0e
Sejal Dhondkar <sejald555@gmail.com>
2021-05-02 09:13:58 +0530

    Guide allotment result admin page simple display of data

--- guideallotmentresults.php
+++ guideallotmentresults.php
@@ -16,51 +16,54 @@
 <body>
     <div class="container-fluid">
         <div class="row">
-            <!-- Sidebar -->
-            <!-- <div class="col-md-2 bg-light d-none d-md-block sidebar">
-                <div class="left-sidebar">
-                    <ul class="nav flex-column sidebar-nav">
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                <svg class="bi bi-chevron-right" width="16" height="16" viewBox="0 0 20 20"
-                                    fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd"
-                                        d="M6.646 3.646a.5.5 0 01.708 0l6 6a.5.5 0 010 .708l-6 6a.5.5 0 01-.708-.708L12.293 10 6.646 4.354a.5.5 0 010-.708z"
-                                        clip-rule="evenodd" /></svg>
-                              <a href="admindashboard.php">Go to Dashboard</a>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                <svg class="bi bi-chevron-right" width="16" height="16" viewBox="0 0 20 20"
-                                    fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd"
-                                        d="M6.646 3.646a.5.5 0 01.708 0l6 6a.5.5 0 010 .708l-6 6a.5.5 0 01-.708-.708L12.293 10 6.646 4.354a.5.5 0 010-.708z"
-                                        clip-rule="evenodd" /></svg>
-                              Enquiry Prediction
-                            </a>
-                        </li>
-						<li class="nav-item">
-                            <a class="nav-link" href="#">
-                                <svg class="bi bi-chevron-right" width="16" height="16" viewBox="0 0 20 20"
-                                    fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd"
-                                        d="M6.646 3.646a.5.5 0 01.708 0l6 6a.5.5 0 010 .708l-6 6a.5.5 0 01-.708-.708L12.293 10 6.646 4.354a.5.5 0 010-.708z"
-                                        clip-rule="evenodd" /></svg>
-                              Enquiry Statistics
-                            </a>
-                        </li> 
-                    </	ul>
-                </div>
-            </div> -->
+           
             <a href="admindashboard.php"><button type="button" class="btn btn-primary" style="margin-top:10px;margin-left:10px;margin-bottom:10px;">Back to Dashboard</button></a>
 
             <main role="main" class=" px-4">
-<center> <h3>Guide Allotment List</h3> </center>
+<center> <h3>Guide Allotment Details</h3> </center>
                 <hr>
+                <form action="guideallotmentresults.php" method="POST">
                 <div class="table-responsive">
                    <center>
+                   <div class="dropdown">
+                     
+                <span>&nbsp;&nbsp;&nbsp;Branch</span>
+                <select id="branch" name="field4">
+                  <!-- <optgroup> -->
+                 
+                    <option value="0"><b>Select Branch</b></option>
+                    <option value=1  <?php $val1=1?>>CMPN</option>
+                    <option value=2  <?php $val1=2?>>IT</option>
+                    <option value=3  <?php $val1=3?>>EXTC</option>
+                    <option value=4  <?php $val1=4?>>ETRX</option>
+                    <option value=5  <?php $val1=5?>>BIOM  </option>
+                  <!-- </optgroup> -->
+                </select>
+               
+               
+                <span>&nbsp;&nbsp;&nbsp;Year</span>
+                <select id="year" name="fieldyear">
+                  <!-- <optgroup> -->
+                    <option value="0"><b>Select Year</b></option>
+
+                    <option value=1>FE</option>
+                    <option value=2>SE</option>
+                    <option value=3>TE</option>
+                    <option value=4>BE</option>
+                  <!-- </optgroup> -->
+                </select>
+
+              
+             
+              
+              
+              <input id="sub1" type="submit" name="submit" value="Submit"/>      
+              </div>      
+              </form> 
+              <?php 
+              
+              
+              ?>
 <style>
  table {
     font-family: arial, sans-serif;
@@ -78,8 +81,35 @@ td, th {
 tr:nth-child(even) {
     background-color: white;
 }
-</style>
+#sub1{
+   
+    margin:5px;
+    margin-left: 20px;
+   padding: 2px;
+}
+.dropdown{
+  margin:10px;
+  display:inline-block;
+}
 
+</style>
+<table>
+
+  <tr>
+    <th>Domain</th>
+   
+    <th>Department</th>
+	<th>Year</th>
+	<th>Guide/Mentor</th>
+  <th>Roll No 1</th>
+	<th>Member 1 (Leader)</th>
+  <th>Roll No 2</th>
+	<th>Member 2</th>
+  <th>Roll No 3</th>
+	<th>Member 3</th>
+  <th>Roll No 4</th>
+	<th>Member 4</th>
+  
 <?php
 $servername = "localhost";
 $username = "root";
@@ -94,77 +124,95 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 //echo "Connected successfully";
-
-
 //displaying data on page from database
-$sql = "SELECT project.project_id, project.project_title, project.year_upload, project.mem_1_lead, project.roll_no_1,
-        project.mem_2, project.roll_no_2, project.mem_3, project.roll_no_3, project.mem_4, project.roll_no_4,project.mem_5, project.roll_no_5,project.mem_6, project.roll_no_6,
-        project.ssummary, project.synopsis_path, project.ppt_path, project.report_path, project.ppt_path, project.publication_path,
-        project.vid_link , project.domain_id, domain.domain_name, project.department_id, department.department_title, project.mentor_id, mentor.mentor_name
-        FROM project, domain, department, mentor
-        WHERE project.domain_id=domain.domain_id AND project.department_id=department.department_id AND project.mentor_id = mentor.mentor_id
-        ORDER BY department_id";
+if(isset($_POST['submit'])){
+  $dept= $_POST['field4'];
+  $yr=$_POST['fieldyear'];
+  $dept=(int)$dept;
+  $yr=(int)$yr;
+  echo $dept,$yr;
+if($dept!=0 && $yr!=0){
+$sql= "SELECT domain.domain_name,student_guide_form.user_id,department.department_title,student_guide_form.year_study,mentor.mentor_name,student_guide_form.mem_1_lead,student_guide_form.mem_2,student_guide_form.mem_3,student_guide_form.mem_4,student_guide_form.roll_no_1,student_guide_form.roll_no_2,student_guide_form.roll_no_3,student_guide_form.roll_no_4
+FROM domain, department, mentor,student_guide_form,alloted_guide 
+WHERE student_guide_form.domain_id=domain.domain_id AND student_guide_form.department_id=department.department_id AND  student_guide_form.user_id=alloted_guide.team_leader_id AND alloted_guide.mentor_id=mentor.mentor_id 
+AND student_guide_form.department_id=$dept AND student_guide_form.year_study=$yr ";}
+elseif($dept==0 && $yr>0){
+  $sql= "SELECT domain.domain_name,student_guide_form.user_id,department.department_title,student_guide_form.year_study,mentor.mentor_name,student_guide_form.mem_1_lead,student_guide_form.mem_2,student_guide_form.mem_3,student_guide_form.mem_4,student_guide_form.roll_no_1,student_guide_form.roll_no_2,student_guide_form.roll_no_3,student_guide_form.roll_no_4
+  FROM project, domain, department, mentor,student_guide_form,alloted_guide 
+  WHERE student_guide_form.domain_id=domain.domain_id AND student_guide_form.department_id=department.department_id AND  student_guide_form.user_id=alloted_guide.team_leader_id AND alloted_guide.mentor_id=mentor.mentor_id 
+  AND  student_guide_form.year_study=$yr ";
+}
+elseif($yr==0 && $dept>0){
+  $sql= "SELECT domain.domain_name,student_guide_form.user_id,department.department_title,student_guide_form.year_study,mentor.mentor_name,student_guide_form.mem_1_lead,student_guide_form.mem_2,student_guide_form.mem_3,student_guide_form.mem_4,student_guide_form.roll_no_1,student_guide_form.roll_no_2,student_guide_form.roll_no_3,student_guide_form.roll_no_4
+  FROM project, domain, department, mentor,student_guide_form,alloted_guide 
+  WHERE student_guide_form.domain_id=domain.domain_id AND student_guide_form.department_id=department.department_id AND  student_guide_form.user_id=alloted_guide.team_leader_id AND alloted_guide.mentor_id=mentor.mentor_id 
+  AND  student_guide_form.department_id=$dept ";
+}
+
+else{
+  $sql= "SELECT domain.domain_name,student_guide_form.user_id,department.department_title,student_guide_form.year_study,mentor.mentor_name,student_guide_form.mem_1_lead,student_guide_form.mem_2,student_guide_form.mem_3,student_guide_form.mem_4,student_guide_form.roll_no_1,student_guide_form.roll_no_2,student_guide_form.roll_no_3,student_guide_form.roll_no_4
+FROM project, domain, department, mentor,student_guide_form,alloted_guide 
+WHERE student_guide_form.domain_id=domain.domain_id AND student_guide_form.department_id=department.department_id AND  student_guide_form.user_id=alloted_guide.team_leader_id AND alloted_guide.mentor_id=mentor.mentor_id 
+ORDER BY  student_guide_form.department_id";
+
+}
+
+
 
 $result = mysqli_query($conn, $sql);
+//$row = mysqli_fetch_array($result);
+//print_r($row);
 
 //}
 
-if ( mysqli_num_rows($result) > 0 ) {
+if ( mysqli_num_rows($result) > 0  ) {
 ?>
-  <table>
+  <!-- <table>
 
   <tr>
     <th>Domain</th>
-    <th>Project Title</th>
+   
     <th>Department</th>
 	<th>Year</th>
 	<th>Guide/Mentor</th>
 	<th>Member 1 (Leader)</th>
 	<th>Member 2</th>
 	<th>Member 3</th>
-	<th>Member 4</th>
-    <th>Member 5</th>
-    <th>Member 6</th>
-	<th>Summary</th>
-    <th>Synopsis</th>
-	<th>Presentation</th>
-	<th>Report</th>
-	<th>Publication</th>
-	<th>Link</th>
-  </tr>
+	<th>Member 4</th> -->
+    
 <?php
+
 $i=0;
 while($row = mysqli_fetch_array($result)) {
 ?>
 <tr>
     <td><?php echo $row["domain_name"]; ?></td>
-    <td><?php echo $row["project_title"]; ?></td>
+   
     <td><?php echo $row["department_title"]; ?></td>
-	<td><?php echo $row["year_upload"]; ?></td>
+	<td><?php echo $row["year_study"]; ?></td>
     <td><?php echo $row["mentor_name"]; ?></td>
-	<td><?php echo $row["mem_1_lead"]; ?> - <?php echo $row["roll_no_1"]; ?></td>
-    <td><?php echo $row["mem_2"]; ?> - <?php echo $row["roll_no_2"]; ?></td>
-    <td><?php echo $row["mem_3"]; ?> - <?php echo $row["roll_no_3"]; ?></td>
-    <td><?php echo $row["mem_4"]; ?> - <?php echo $row["roll_no_4"]; ?></td>
-    <td><?php echo $row["mem_5"]; ?> - <?php echo $row["roll_no_5"]; ?></td>
-    <td><?php echo $row["mem_6"]; ?> - <?php echo $row["roll_no_6"]; ?></td>
-    <td><?php echo $row["ssummary"]; ?></td>
-    <td><a href="synopsis/<?php echo htmlentities($row['synopsis_path']); ?>">Download Synopsis</a></td>
-	<td><a href="ppt/<?php echo htmlentities($row['ppt_path']); ?>">Download PPT</a></td>
-    <td><a href="report/<?php echo htmlentities($row['report_path']); ?>">Download Report</a></td>
-    <td><a href="papers/<?php echo htmlentities($row['publication_path']); ?>">Download Papers</a></td>
-    <td><?php echo '<a href="'.$row["vid_link"].'" target="_blank">Watch Video</a>'; ?></td>
+    <td><?php echo $row["roll_no_1"]; ?> 
+	<td><?php echo $row["mem_1_lead"]; ?>
+  <td><?php echo $row["roll_no_2"]; ?> 
+    <td><?php echo $row["mem_2"]; ?> 
+    <td><?php echo $row["roll_no_3"]; ?> 
+    <td><?php echo $row["mem_3"]; ?> 
+    <td><?php echo $row["roll_no_4"]; ?> 
+    <td><?php echo $row["mem_4"]; ?> 
+    
+   
 </tr>
 <?php
 $i++;
 }
-echo "Total Project entries are". "\t" . $i;
+echo "Total allotment entries are". "\t" . $i;
 ?>
 </table>
  <?php
 }
 else{
-    echo "No result found";
+    echo "No entries found";
+}
 }
 ?>
  </body>
